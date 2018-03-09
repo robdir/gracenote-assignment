06171603cea2c54c98af1563521b9a702336101a
robdir <rhwickham@gmail.com>
2018-03-09 14:31:36 +0100

    speed skating table rendered

--- src/speedTable.js
+++ src/speedTable.js
@@ -62,6 +62,96 @@ class speedTable extends PureComponent {
                 <td> {data.GoldMedalList[7].Gender.c_Short} </td>
                 <td> Gold </td>
             </tr>
+                <tr>
+                    <td> {data.SilverMedalList[0].Participant.c_Participant}</td>
+                    <td> {data.SilverMedalList[0].Event.c_Name} </td>
+                    <td> {data.SilverMedalList[0].Gender.c_Short} </td>
+                    <td> Silver </td>
+                </tr>
+                <tr>
+                    <td> {data.SilverMedalList[1].Participant.c_Participant}</td>
+                    <td> {data.SilverMedalList[1].Event.c_Name} </td>
+                    <td> {data.SilverMedalList[1].Gender.c_Short} </td>
+                    <td> Silver </td>
+                </tr>
+                <tr>
+                    <td> {data.SilverMedalList[2].Participant.c_Participant}</td>
+                    <td> {data.SilverMedalList[2].Event.c_Name} </td>
+                    <td> {data.SilverMedalList[2].Gender.c_Short} </td>
+                    <td> Silver </td>
+                </tr>
+                <tr>
+                    <td> {data.SilverMedalList[3].Participant.c_Participant}</td>
+                    <td> {data.SilverMedalList[3].Event.c_Name} </td>
+                    <td> {data.SilverMedalList[3].Gender.c_Short} </td>
+                    <td> Silver </td>
+                </tr>
+                <tr>
+                    <td> {data.SilverMedalList[4].Participant.c_Participant}</td>
+                    <td> {data.SilverMedalList[4].Event.c_Name} </td>
+                    <td> {data.SilverMedalList[4].Gender.c_Short} </td>
+                    <td> Silver </td>
+                </tr>
+                <tr>
+                    <td> {data.SilverMedalList[5].Participant.c_Participant}</td>
+                    <td> {data.SilverMedalList[5].Event.c_Name} </td>
+                    <td> {data.SilverMedalList[5].Gender.c_Short} </td>
+                    <td> Silver </td>
+                </tr>
+                <tr>
+                    <td> {data.SilverMedalList[6].Participant.c_Participant}</td>
+                    <td> {data.SilverMedalList[6].Event.c_Name} </td>
+                    <td> {data.SilverMedalList[6].Gender.c_Short} </td>
+                    <td> Silver </td>
+                </tr>
+                <tr>
+                    <td> {data.BronzeMedalList[0].Participant.c_Participant}</td>
+                    <td> {data.BronzeMedalList[0].Event.c_Name} </td>
+                    <td> {data.BronzeMedalList[0].Gender.c_Short} </td>
+                    <td> Bronze </td>
+                </tr>
+                <tr>
+                    <td> {data.BronzeMedalList[1].Participant.c_Participant}</td>
+                    <td> {data.BronzeMedalList[1].Event.c_Name} </td>
+                    <td> {data.BronzeMedalList[1].Gender.c_Short} </td>
+                    <td> Bronze </td>
+                </tr>
+                <tr>
+                    <td> {data.BronzeMedalList[2].Participant.c_Participant}</td>
+                    <td> {data.BronzeMedalList[2].Event.c_Name} </td>
+                    <td> {data.BronzeMedalList[2].Gender.c_Short} </td>
+                    <td> Bronze </td>
+                </tr>
+                <tr>
+                    <td> {data.BronzeMedalList[3].Participant.c_Participant}</td>
+                    <td> {data.BronzeMedalList[3].Event.c_Name} </td>
+                    <td> {data.BronzeMedalList[3].Gender.c_Short} </td>
+                    <td> Bronze </td>
+                </tr>
+                <tr>
+                    <td> {data.BronzeMedalList[4].Participant.c_Participant}</td>
+                    <td> {data.BronzeMedalList[4].Event.c_Name} </td>
+                    <td> {data.BronzeMedalList[4].Gender.c_Short} </td>
+                    <td> Bronze </td>
+                </tr>
+                <tr>
+                    <td> {data.BronzeMedalList[5].Participant.c_Participant}</td>
+                    <td> {data.BronzeMedalList[5].Event.c_Name} </td>
+                    <td> {data.BronzeMedalList[5].Gender.c_Short} </td>
+                    <td> Bronze </td>
+                </tr>
+                <tr>
+                    <td> {data.BronzeMedalList[6].Participant.c_Participant}</td>
+                    <td> {data.BronzeMedalList[6].Event.c_Name} </td>
+                    <td> {data.BronzeMedalList[6].Gender.c_Short} </td>
+                    <td> Bronze </td>
+                </tr>
+                <tr>
+                    <td> {data.BronzeMedalList[7].Participant.c_Participant}</td>
+                    <td> {data.BronzeMedalList[7].Event.c_Name} </td>
+                    <td> {data.BronzeMedalList[7].Gender.c_Short} </td>
+                    <td> Bronze </td>
+                </tr>
             </table>
         )
     }
