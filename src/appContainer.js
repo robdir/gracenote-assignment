3b78b9ab9b892095a695e351e5479e188c6c1a24
robdir <rhwickham@gmail.com>
2018-03-09 12:11:56 +0100

    first table created

--- src/appContainer.js
+++ src/appContainer.js
@@ -4,8 +4,32 @@ import {connect} from 'react-redux'
 
 class appContainer extends PureComponent {
     render() {
+
+        const {sportData} = this.props
+
         return (
-            <p> Hi </p>
+            <div className="main_container">
+                <div className="intro">
+                    <h3> An overview of the Dutch performance in the 2014 Winter Olympics </h3>
+                </div>
+
+                <table className="total_medals">
+                    <tr>
+                        <th> Country </th>
+                        <th> Total Medals Won </th>
+                        <th> Gold </th>
+                        <th> Silver </th>
+                        <th> Bronze </th>
+                    </tr>
+                    <tr>
+                        <td> {sportData.NOCMedals.NOC.c_Name} </td>
+                        <td> {sportData.NOCMedals.Medals.n_Total} </td>
+                        <td> {sportData.NOCMedals.Medals.n_Gold} </td>
+                        <td> {sportData.NOCMedals.Medals.n_Silver} </td>
+                        <td> {sportData.NOCMedals.Medals.n_Bronze} </td>
+                    </tr>
+                </table>
+            </div>
         )
     }
 }
@@ -13,3 +37,4 @@ class appContainer extends PureComponent {
 const mapStatetoProps = ({ sportData }) => ({ sportData })
 
 export default connect(mapStatetoProps)(appContainer)
+
--- src/index.js
+++ src/index.js
@@ -5,7 +5,6 @@ import store from './store';
 import './index.css';
 import App from './App';
 
-ReactDOM.render
-    (<Provider store={store}>
+ReactDOM.render    (<Provider store={store}>
         <App />
     </Provider>, document.getElementById('root'));
