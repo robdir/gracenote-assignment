06ff664a76719c34ad4b8e0b855670f92d66d7ea
robdir <rhwickham@gmail.com>
2018-03-09 15:28:25 +0100

    testing with redux

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { shallow } from 'enzyme';
+import Enzyme from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16'
 import App from './App';
+import Container from './appContainer'
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
-});
+Enzyme.configure({ adapter: new Adapter() })
+
+describe('<App />', () => {
+  const app = shallow(<App/>)
+  const container = shallow(<Container/>)
+
+  it('is rendered within a div tag', () => {
+    expect(app).toHaveTagName('div')
+  })
+})
--- src/appContainer.js
+++ src/appContainer.js
@@ -11,7 +11,7 @@ class appContainer extends PureComponent {
         return (
             <div className="main_container">
                 <div className="intro">
-                    <h3> An overview of the Dutch
+                    <h3> An overview of Dutch
                          performance in the 2014 Winter Olympics
                     </h3>
                 </div>
@@ -39,42 +39,68 @@ class appContainer extends PureComponent {
                     </p>
                 </div>
 
-                <h3> Sport-Type </h3>
+                <div className="table_1">
 
-                <table className="medal_breakdown">
-                    <tr>
-                        <th> Type  </th>
-                        <th> Total Medals Won </th>
-                        <th> Gold </th>
-                        <th> Silver </th>
-                        <th> Bronze </th>
-                    </tr>
-                    <tr>
-                        <td> {sportData.SportList[0].Sport.c_Name} </td>
-                        <td> {sportData.SportList[0].Medals.n_Total} </td>
-                        <td> {sportData.SportList[0].Medals.n_Gold} </td>
-                        <td> {sportData.SportList[0].Medals.n_Silver} </td>
-                        <td> {sportData.SportList[0].Medals.n_Bronze} </td>
-                    </tr>
-                    <tr>
-                        <td> {sportData.SportList[1].Sport.c_Name} </td>
-                        <td> {sportData.SportList[1].Medals.n_Total} </td>
-                        <td> {sportData.SportList[1].Medals.n_Gold} </td>
-                        <td> {sportData.SportList[1].Medals.n_Silver} </td>
-                        <td> {sportData.SportList[1].Medals.n_Bronze} </td>
-                    </tr>
-                </table>
+                    <h3> Sport-Type </h3>
 
-                <h3> Participants - {sportData.SportList[0].Sport.c_Name} </h3>
-
-                <div className="participants">
-
-                <SpeedTable data={sportData.SportList[0]}/>
+                    <table className="medal_breakdown">
+                        <tr>
+                            <th> Type  </th>
+                            <th> Total Medals Won </th>
+                            <th> Gold </th>
+                            <th> Silver </th>
+                            <th> Bronze </th>
+                        </tr>
+                        <tr>
+                            <td> {sportData.SportList[0].Sport.c_Name} </td>
+                            <td> {sportData.SportList[0].Medals.n_Total} </td>
+                            <td> {sportData.SportList[0].Medals.n_Gold} </td>
+                            <td> {sportData.SportList[0].Medals.n_Silver} </td>
+                            <td> {sportData.SportList[0].Medals.n_Bronze} </td>
+                        </tr>
+                        <tr>
+                            <td> {sportData.SportList[1].Sport.c_Name} </td>
+                            <td> {sportData.SportList[1].Medals.n_Total} </td>
+                            <td> {sportData.SportList[1].Medals.n_Gold} </td>
+                            <td> {sportData.SportList[1].Medals.n_Silver} </td>
+                            <td> {sportData.SportList[1].Medals.n_Bronze} </td>
+                        </tr>
+                    </table>
 
                 </div>
 
+                <div className="table_2">
 
-            </div>
+                    <h3> Participants - {sportData.SportList[0].Sport.c_Name} </h3>
+
+                    <div>
+                        <SpeedTable data={sportData.SportList[0]}/>
+                    </div>
+                </div>
+
+                <div className="table_3">
+
+                    <h3> Participants - {sportData.SportList[1].Sport.c_Name} </h3>
+
+                    <table className="medal_breakdown">
+                        <tr>
+                            <th> Participant Name </th>
+                            <th> Event </th>
+                            <th> Gender </th>
+                            <th> Medal Type </th>
+                        </tr>
+                        <tr>
+                            <td> {sportData.SportList[1].BronzeMedalList[0].Participant.c_Participant} </td>
+                            <td> {sportData.SportList[1].BronzeMedalList[0].Event.c_Name} </td>
+                            <td> {sportData.SportList[1].BronzeMedalList[0].Gender.c_Short} </td>
+                            <td> Bronze </td>
+                        </tr>
+                    </table>
+                </div>
+            
+                <h4> That's all folks! </h4>
+
+            </div>  
         )
     }
 }
