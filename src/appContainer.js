70193c93bb89f57de7909ac1f210db62899643b9
robdir <rhwickham@gmail.com>
2018-03-09 12:31:29 +0100

    medal breakdown rendered

--- src/appContainer.js
+++ src/appContainer.js
@@ -10,7 +10,9 @@ class appContainer extends PureComponent {
         return (
             <div className="main_container">
                 <div className="intro">
-                    <h3> An overview of the Dutch performance in the 2014 Winter Olympics </h3>
+                    <h3> An overview of the Dutch
+                         performance in the 2014 Winter Olympics
+                    </h3>
                 </div>
 
                 <table className="total_medals">
@@ -29,6 +31,40 @@ class appContainer extends PureComponent {
                         <td> {sportData.NOCMedals.Medals.n_Bronze} </td>
                     </tr>
                 </table>
+            
+                <div className="description">
+                    <p> Below you can find a more detailed breakdown of
+                        medal winnings, listed by sport type and participants. 
+                    </p>
+                </div>
+
+                <h3> Sport-Type </h3>
+
+                <table className="medal_breakdown">
+                    <tr>
+                        <th> Type  </th>
+                        <th> Total Medals Won </th>
+                        <th> Gold </th>
+                        <th> Silver </th>
+                        <th> Bronze </th>
+                    </tr>
+                    <tr>
+                        <td> {sportData.SportList[0].Sport.c_Name} </td>
+                        <td> {sportData.SportList[0].Medals.n_Total} </td>
+                        <td> {sportData.SportList[0].Medals.n_Gold} </td>
+                        <td> {sportData.SportList[0].Medals.n_Silver} </td>
+                        <td> {sportData.SportList[0].Medals.n_Bronze} </td>
+                    </tr>
+                    <tr>
+                        <td> {sportData.SportList[1].Sport.c_Name} </td>
+                        <td> {sportData.SportList[1].Medals.n_Total} </td>
+                        <td> {sportData.SportList[1].Medals.n_Gold} </td>
+                        <td> {sportData.SportList[1].Medals.n_Silver} </td>
+                        <td> {sportData.SportList[1].Medals.n_Bronze} </td>
+                    </tr>
+                </table>
+
+
             </div>
         )
     }
